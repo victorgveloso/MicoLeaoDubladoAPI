ce3b6c4a7a1b6a125ea75cf1697931cacdd16667
Victor Guerra Veloso <victorgvbh@gmail.com>
2021-05-24 23:24:48 -0300

    Removed series model (it is redundant with stream)

--- test/persistence/models/series.test.js
+++ test/persistence/models/series.test.js
@@ -1,9 +1,8 @@
-const SeriesStreamModel = require("../../../src/persistence/models/series").default;
-
+const Stream = require("../../../src/persistence/models/stream").default;
 describe("Given a series",()=>{
     let series;
     beforeEach(()=>{
-        series = new SeriesStreamModel({metaId: "tt9140560", episode: 1, season: 1});
+        series = new Stream({metaId: "tt9140560", type: "series", title: "WandaVision", episode: 1, season: 1});
     })
     it("Should contain episode and season specified in its stream id", () => {
         expect(series.streamId).toEqual(`${series.metaId}:${series.episode}:${series.season}`);
